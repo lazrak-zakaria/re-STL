4033bbd214d2e8ad277f16f977a6595d0237e6d9
lazrak-zakaria <z.zakarialazrak@gmail.com>
2025-10-05 21:48:58 +0100

    rb iterator

--- test/rb_tree/main.cpp
+++ test/rb_tree/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "../../rb_tree.hpp"
+#include <set>
+
 
 #include <vector>
-
+using namespace std;
 class m
 {
 public:
@@ -24,18 +26,53 @@ public:
         std::cout << "Destructor called" << std::endl;
     }
 };
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
 #include <iostream>
 #include <cstdlib>
 #include <ctime>    
 int main() {
     rb_tree<int> rb;
 
-    std::srand(std::time(nullptr)); 
-    for (int i = 0; i < 50; ++i) {
+    set<int> st;
+
+
+    for (int i = 0; i < 50000; ++i) {
         int value = i; 
         rb.insert(value);
     }
-    std::cout << rb.delete_node(40) << "\n";
-    rb.print();
+    for (int i = 0; i < 50000; ++i) {
+        int value = i; 
+        st.insert(value);
+        
+    }
+    auto start1 = high_resolution_clock::now();
+
+    rb.find(50000 -500);
+    auto end1 = high_resolution_clock::now();
+
+    // --- Second operation ---
+    auto start2 = high_resolution_clock::now();
+    st.count(50000 -500) ;
+    auto end2 = high_resolution_clock::now();
+
+    // --- Calculate durations ---
+    auto duration1 = duration_cast<microseconds>(end1 - start1).count();
+    auto duration2 = duration_cast<microseconds>(end2 - start2).count();
+
+
+    cout << "Operation 1 took: " << duration1 << " microseconds\n";
+    cout << "Operation 2 took: " << duration2 << " microseconds\n";
+
+    // std::srand(std::time(nullptr)); 
+    // for (int i = 0; i < 50000; ++i) {
+    //     int value = i; 
+    //     rb.insert(value);
+    // }
+    // std::cout << rb.delete_node(40) << "\n";
+    // rb.print();
     return 0;
 }
