377b926b72b2ec7cb052bb11c214ac6e49f63c32
lazrak-zakaria <z.zakarialazrak@gmail.com>
2025-10-09 02:29:30 +0100

    pq functions in algorithm hpp / pq need tests

--- algorithm.hpp
+++ algorithm.hpp
@@ -1,35 +1,103 @@
+#include <cstddef>
 #ifndef FT_ALGORITHM_HPP__
 
 #define FT_ALGORITHM_HPP__
 
-template <class RandomAccessIterator>
-void make_heap(RandomAccessIterator first, RandomAccessIterator last)
-{
-}
+// i suppose i get a random_access_iterator_tag
 
-template <class RandomAccessIterator, class Compare>
-void make_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
-{
-}
-
-template <class RandomAccessIterator>
-void push_heap(RandomAccessIterator first, RandomAccessIterator last)
-{
-}
-
-template <class RandomAccessIterator, class Compare>
-void push_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
-{
-}
-
-template <class RandomAccessIterator>  void pop_heap (RandomAccessIterator first, RandomAccessIterator last)
+namespace ft
 {
 
-}
+    template <class RandomAccessIterator>
+    void make_heap(RandomAccessIterator first, RandomAccessIterator last)
+    {
+    }
 
-template <class RandomAccessIterator, class Compare>  void pop_heap (RandomAccessIterator first, RandomAccessIterator last,                 Compare comp)
-{
-    
+    template <class RandomAccessIterator, class Compare>
+    void make_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+    {
+        if (first == last)
+            return;
+        size_t pos = last - first - 1;
+        while (pos >= 0)
+        {
+            heapify_down(first, last, comp, pos)
+                pos -= 1;
+        }
+    }
+
+    template <class RandomAccessIterator>
+    void push_heap(RandomAccessIterator first, RandomAccessIterator last)
+    {
+        pop_heap(first, second, std::less<typename RandomAccessIterator::value_type>);
+    }
+
+    template <class T>
+    void swap(T &a, T &b)
+    {
+        T tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    template <class RandomAccessIterator, class Compare>
+    void push_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+    {
+
+        if (first == last)
+            return;
+
+        size_t pos = last - first - 1;
+        while (pos)
+        {
+            size_t parent = (pos - 1) / 2;
+            if (comp(first[pos], first[parent]))
+                return;
+            ft::swap(first[parent], first[pos]);
+            pos = parent;
+        }
+    }
+
+    template <class RandomAccessIterator>
+    void pop_heap(RandomAccessIterator first, RandomAccessIterator last)
+    {
+        pop_heap(first, last, std::less<typename RandomAccessIterator::value_type>);
+    }
+
+    template <class RandomAccessIterator, class Compare>
+    void pop_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+    {
+        if (first == last)
+            return;
+
+        ft::swap(*first, *(last - 1));
+        heapify_down(first, last - 1, comp, 0);
+    }
+
+    template <class RandomAccessIterator, class Compare>
+    void heapify_down(RandomAccessIterator first, RandomAccessIterator last, Compare comp, size_t pos)
+    {
+        size_t sz = last - first;
+        size_t child_pos = 0;
+
+        while (pos < sz)
+        {
+            size_t left_child = pos * 2 + 1;
+            size_t right_child = pos * 2 + 2;
+
+            if (left_child >= sz)
+                return;
+
+            child_pos = left_child;
+            if (right_child < sz) // if right exists i choose the bigger one;
+                child_pos = comp(c[left_child], c[right_child]) ? right_child : left_child;
+
+            if (comp(c[child_pos], c[pos])) // default it means if child is less than me : so i return
+                return;
+
+            ft::swap(first[child_pos], first[pos]);
+        }
+    }
 }
 
 #endif
--- queue.hpp
+++ queue.hpp
@@ -12,7 +12,7 @@ formula   2*i+1
 its parent =floor ((i-1/2))
 
 */
-
+#include "algorithm.hpp"
 #include <vector>
 
 namespace ft
@@ -32,98 +32,50 @@ namespace ft
         value_compare cmp;
 
     private:
-        size_t parent(size_t i)
+    public:
+        explicit priority_queue(const Compare &comp = Compare(), const Container &ctnr = Container())::cmp(comp), c(cntr)
         {
-            if (!i || i >= c.size())
-                return -1;
-            return (i - 1) / 2
-        }
-        size_t child_left(size_t i)
-        {
-            if (!i || i >= c.size())
-                return -1;
-            return 2 * i + 1;
-        }
-        size_t child_right(size_t i)
-        {
-            if (!i || i >= c.size())
-                return -1;
-            return 2 * i + 2;
+
+            ft::make_heap(c.begin(), c.end());
         }
 
-        void heapify_up(size_t node)
+        template <class InputIterator>
+        priority_queue(InputIterator first, InputIterator last, const Compare &comp = Compare(),
+                       const Container &ctnr = Container()) : cmp(comp), c(cntr)
         {
-            if (!node || node >= c.size())
-                return;
-            size_t parent_ = parent(node);
-            if (parent_ < c.size() && cmp(c[node], c[parent_]))
-                return;
-            ////swap
-            value_type t = c[parent_];
-            c[parent_] = c[node];
-            c[node] = t;
-
-            heapify_up(parent_);
+            while (first != last)
+            {
+                c.push_back(*first);
+                first++;
+            }
+            ft::make_heap(c.begin(), c.end());
         }
 
-        void heapify_down(size_t node)
+        void push(const value_type &val)
         {
-            if (node >= c.size())
-                return;
-
-            size_t left_child = child_left(node);
-            size_t right_child = child_right(node);
-
-            size_t child_pos = -1;
-            // i need the min one
-
-            if (left_child == child_pos) // no childs
-                return;
-
-            child_pos = right_child != child_pos && cmp(c[left_child], c[right_child]) ? right_child : left_child;
-
-            if (cmp(c[child_pos], c[node]))
-                return;
-
-            ////swap
-            value_type t = c[child_pos];
-            c[child_pos] = c[node];
-            c[node] = t;
-
-            heapify_down(child_pos);
+            c.push_back(val);
+            push_heap(c.begin(), c.end(), cmp);
         }
 
-        public:
-            void  push (const value_type& val)
-            {
-                c.push_back(val);
-                heapify_up(c.size()-1);
-            }
+        void pop()
+        {
+            ft::pop_heap(c.begin(), c.end(), cmp);
+            c.pop_back();
+        }
+        bool empty()
+        {
+            return !c.size();
+        }
 
-            void pop ()
-            {
-                if (empty())
-                    return ;
+        const value_type &top() const
+        {
+            return c.front();
+        }
 
-                c[0] = c[c.size()-1];
-
-                c.pop_back();
-                heapify_down(0);
-            }
-            bool empty()
-            {
-                return ! c.size();
-            }
-
-            const value_type& top() const
-            {
-                return c.front();
-            }
-
-            size_type size() const
-            {
-                return c.size();
-            }
+        size_type size() const
+        {
+            return c.size();
+        }
     };
 
 }
