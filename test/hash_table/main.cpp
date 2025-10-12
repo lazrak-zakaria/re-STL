b07df164f8d28ce818530d9df11407075498d9ea
lazrak-zakaria <z.zakarialazrak@gmail.com>
2025-10-12 19:33:58 +0100

    fix

--- test/hash_table/main.cpp
+++ test/hash_table/main.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-// #include "../../hash_table.hpp"
+#include "../../hash_table.hpp"
 
 #include <iostream>
 #include <unordered_set>
@@ -12,61 +12,6 @@ int main()
 
     const int N = 10000000; 
 
-
-    // {
-    //     ft::hash_table<int> ht;
-
-    //     auto start = high_resolution_clock::now();
-
-    //     for (int i = 0; i < N; ++i)
-    //         ht._insert(i);
-
-    //     auto end = high_resolution_clock::now();
-    //     auto duration = duration_cast<milliseconds>(end - start);
-
-    //     std::cout << "ft::hash_table insert time: " << duration.count() << " ms\n";
-    //     start = high_resolution_clock::now();
-    //     for (size_t i = 0; i < ht.table.size(); ++i)
-    //     {
-    //         ft::hash_node<int> *ptr = ht.table[i];
-    //         while (ptr)
-    //         {   if (ptr->key < 40 )
-    //             std::cout <<  ptr->key << "\n";
-    //             ptr = ptr->next;
-    //         }
-    //     }
-
-    //     end = high_resolution_clock::now();
-    //     std::cout << "ft::hash_table traversal time: "
-    //               << duration_cast<milliseconds>(end - start).count() << " ms\n";
-    // }
-
-
-    {
-        std::unordered_set<int> uset;
-
-        // auto start = high_resolution_clock::now();
-        uset.max_load_factor(10);
-        for (int i = 0; i < 6; ++i)
-        {
-            std::cout <<uset.bucket_count() << "\n";
-            uset.insert(i);
-        }
-        std::cout << "---------------\n";
-
-        // for (int i = 0; i < 10; ++i)
-        // {
-        //     std::cout <<uset.bucket_count() << "\n";
-        //     uset.erase(uset.begin());
-        // }
-        std::unordered_set<int> :: local_iterator it = uset.end(500);
-        it++;
-        
-
-
-    // auto start = high_resolution_clock::now();
-
-    }
 
     return 0;
 }
